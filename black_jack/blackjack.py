d2b8022fcc764b7462d49d727eabd51ab8ce13cb
Quyen Mac <qmac@Quyen-Quyen-Macs-MacBook-Pro.local>
2016-03-03 18:55:51 -0500

    added structure

---
+++ black_jack/blackjack.py
@@ -0,0 +1,22 @@
+import random
+import itertools
+
+Ranks = '23456789TJKQKA'
+
+
+
+#functions
+def deal():
+    pass
+
+def hit():
+    pass
+
+def bust():
+    pass
+
+def newGame():
+    pass
+
+def main():
+    pass
